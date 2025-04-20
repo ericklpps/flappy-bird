32fe9287bb5b3380ca01ea0696f2a963129f5266
ericklpps <ericklpsilva@gmail.com>
2025-04-20 17:59:55 -0300

    feat: adicionando cenário

--- script.js
+++ script.js
@@ -6,7 +6,56 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        )
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        )
+    }
+}
 
+
+const chao = {
+    spriteX: 0,
+    spriteY: 610, 
+    largura: 224,
+    altura: 112, 
+    x: 0,
+    y: canvas.height - 112,
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+        );
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,  
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura,
+        );
+    }
+};
 
 const flappyBird = {
     spriteX: 0,
@@ -31,8 +80,10 @@ function loop(){
 //algo na tela, no caso, o conteúdo abaixo    
 
     flappyBird.desenha(); //Quando quiser desenhar o flappy bird na tela
+    planoDeFundo.desenha();
+    chao.desenha();
+    
     requestAnimationFrame(loop); //ajuda a desenhar os quadros na tela infinitamente
-
 }
 
 loop();
